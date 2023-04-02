eff6ef1953c883d57a55ef58d9176c2c39a3ca1c
Kanna <126311660+eSportxD@users.noreply.github.com>
2023-04-02 23:50:59 +0530

    Update __init__.py

--- Pokemonxd/resource/__init__.py
+++ Pokemonxd/resource/__init__.py
@@ -1,8 +1,11 @@
 import os
 
+thumbs = []
+
 colors = ["white", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
 for filename in os.listdir("./Pokemonxd/resource"):
 
     if filename.endswith("png"):
 
+        thumbs.append(filename[:-4])
