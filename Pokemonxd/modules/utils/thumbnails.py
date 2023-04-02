f5ce29a8bf8a811f1157c636ddd7b4c6bc6175ca
Kanna <126311660+eSportxD@users.noreply.github.com>
2023-04-02 23:34:17 +0530

    Update thumbnails.py

--- Pokemonxd/modules/utils/thumbnails.py
+++ Pokemonxd/modules/utils/thumbnails.py
@@ -3,18 +3,19 @@
 import os
 import re
 import textwrap
-import random
+
 import aiofiles
 import aiohttp
-from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
-                 ImageFont, ImageOps)
+import numpy as np
+import random
+
+from PIL import Image, ImageChops, ImageOps, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from youtubesearchpython.__future__ import VideosSearch
 
-from Pokemonxd.utilities.config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+from Pokemonxd.utilities.config import YOUTUBE_IMG_URL
+from Pokemonxd.resource import  colors
 
 
-themes = ["bgreen", "blue", "colorfull", "dgreen", "hgreen", "lgreen", "lyellow", "orange", "pink", "purple", "rainbow", "red", "sky", "thumbnail", "yellow"]
-colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
@@ -25,10 +26,18 @@ def changeImageSize(maxWidth, maxHeight, image):
     return newImage
 
 
+def add_corners(im):
+    bigsize = (im.size[0] * 3, im.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    ImageDraw.Draw(mask).ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(im.size, Image.ANTIALIAS)
+    mask = ImageChops.darker(mask, im.split()[-1])
+    im.putalpha(mask)
+
+
 async def gen_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
-
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
@@ -42,49 +51,121 @@ async def gen_thumb(videoid):
             try:
                 duration = result["duration"]
             except:
-                duration = "Unknown Mins"
+                duration = "Unknown"
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
             try:
-                views = result["viewCount"]["short"]
+                result["viewCount"]["short"]
             except:
-                views = "Unknown Views"
+                pass
             try:
-                channel = result["channel"]["name"]
+                result["channel"]["name"]
             except:
-                channel = "Unknown Channel"
+                pass
 
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(
-                        f"cache/thumb{videoid}.png", mode="wb"
-                    )
+                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
                     await f.write(await resp.read())
                     await f.close()
 
-        images = random.choice(themes)
+        xy = Image.open(wxy)
+        a = Image.new('L', [640, 640], 0)
+        b = ImageDraw.Draw(a)
+        b.pieslice([(0, 0), (640,640)], 0, 360, fill = 255, outline = "white")
+        c = np.array(xy)
+        d = np.array(a)
+        e = np.dstack((c, d))
+        f = Image.fromarray(e)
+        x = f.resize((107, 107))
+
         border = random.choice(colors)
-        image1 = Image.open(f"cache/thumb{videoid}.png")
-        image2 = Image.open(f"Pokemonxd/resource/{images}.png")
-        image3 = changeImageSize(1280, 720, image1)
-        image4 = changeImageSize(1280, 720, image2)
+        youtube = Image.open(f"cache/thumb{videoid}.png")
+        bg = Image.open(f"Pokemonxd/resource/amala.png")
+        image1 = changeImageSize(1280, 720, youtube)
+        image2 = image1.convert("RGBA")
+        background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        enhancer = ImageEnhance.Brightness(background)
+        background = enhancer.enhance(0.6)
+
+        image3 = changeImageSize(1280, 720, bg)
         image5 = image3.convert("RGBA")
-        image6 = image4.convert("RGBA")
-        Image.alpha_composite(image5, image6).save("cache/temp.png")
-        logo = Image.open("cache/temp.png")
-        img = ImageOps.expand(logo, border=10, fill=f"{border}")
+        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
+
+        Xcenter = youtube.width / 2
+        Ycenter = youtube.height / 2
+        x1 = Xcenter - 250
+        y1 = Ycenter - 250
+        x2 = Xcenter + 250
+        y2 = Ycenter + 250
+        logo = youtube.crop((x1, y1, x2, y2))
+        logo.thumbnail((520, 520), Image.ANTIALIAS)
+        logo.save(f"cache/chop{videoid}.png")
+        if not os.path.isfile(f"cache/cropped{videoid}.png"):
+            im = Image.open(f"cache/chop{videoid}.png").convert("RGBA")
+            add_corners(im)
+            im.save(f"cache/cropped{videoid}.png")
+
+        crop_img = Image.open(f"cache/cropped{videoid}.png")
+        logo = crop_img.convert("RGBA")
+        logo.thumbnail((365, 365), Image.ANTIALIAS)
+        width = int((1280 - 365) / 2)
+        background = Image.open(f"cache/temp{videoid}.png")
+        background.paste(logo, (width + 2, 138), mask=logo)
+        background.paste(x, (710, 427), mask=x)
+        background.paste(image3, (0, 0), mask=image3)
+        img = ImageOps.expand(background, border=10, fill=f"{border}")
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("Pokemonxd/resource/font.otf", 32)
-        draw.text((190, 555), f"Title: {title[:50]} ...", (255, 255, 255), font=font)
-        draw.text((190, 595), f"Duration: {duration}", (255, 255, 255), font=font)
-        draw.text((190, 635), f"Views: {views}", (255, 255, 255), font=font)
-        draw.text((190, 675), f"Powered By: Blackcat (@pokemonnxd)", (255, 255, 255), font=font)
+        font = ImageFont.truetype("Pokemonxd/resource/font2.ttf", 45)
+        ImageFont.truetype("Pokemonxd/resource/font2.ttf", 70)
+        arial = ImageFont.truetype("Pokemonxd/resource/font2.ttf", 30)
+        ImageFont.truetype("Pokemonxd/resource/font.ttf", 30)
+        para = textwrap.wrap(title, width=32)
+        try:
+            draw.text(
+                (450, 35),
+                f"STARTED PLAYING",
+                fill="white",
+                stroke_width=1,
+                stroke_fill="white",
+                font=font,
+            )
+            if para[0]:
+                text_w, text_h = draw.textsize(f"{para[0]}", font=font)
+                draw.text(
+                    ((1280 - text_w) / 2, 560),
+                    f"{para[0]}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="white",
+                    font=font,
+                )
+            if para[1]:
+                text_w, text_h = draw.textsize(f"{para[1]}", font=font)
+                draw.text(
+                    ((1280 - text_w) / 2, 610),
+                    f"{para[1]}",
+                    fill="white",
+                    stroke_width=1,
+                    stroke_fill="white",
+                    font=font,
+                )
+        except:
+            pass
+        text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
+        draw.text(
+            ((1280 - text_w) / 2, 665),
+            f"Duration: {duration} Mins",
+            fill="white",
+            font=arial,
+        )
         try:
             os.remove(f"cache/thumb{videoid}.png")
-            os.remove(f"cache/temp.png")
         except:
             pass
         img.save(f"cache/{videoid}.png")
         return f"cache/{videoid}.png"
-    except Exception:
+    except Exception as e:
+        print(e)
         return YOUTUBE_IMG_URL
+
