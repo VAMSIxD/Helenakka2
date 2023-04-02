eb1c5aefae8c3d9ad50585a430a2c0c3ac340a79
Kanna <126311660+eSportxD@users.noreply.github.com>
2023-04-02 23:37:16 +0530

    Add files via upload

---
+++ Pokemonxd/resource/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+colors = ["white", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
+
+for filename in os.listdir("./Pokemonxd/resource"):
+
+    if filename.endswith("png"):
+
+        thumbs.append(filename[:-4])
