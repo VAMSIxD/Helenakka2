b189411c45430c720bbe4c5c5833cb7f0c4f3cc6
VAMSI REDDY <129572799+VAMSIxD@users.noreply.github.com>
2023-08-24 17:57:19 +0530

    Update play.py

--- Pokemonxd/utilities/inline/play.py
+++ Pokemonxd/utilities/inline/play.py
@@ -39,7 +39,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥀 ❰ 𝐎𝐰𝐧𝐞𝐫シ︎𝐱𝐃 ❱ ✨", url=f"https://t.me/shreya_ghoshal_143"), 
+                text="🥀 ❰ 𝐎𝐰𝐧𝐞𝐫シ︎𝐱𝐃 ❱ ✨", url=f"https://t.me/ForeverMine102"), 
         ],
         [
             InlineKeyboardButton(
@@ -123,7 +123,7 @@ def telegram_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🥀 ❰ 𝐎𝐰𝐧𝐞𝐫シ︎𝐱𝐃 ❱ ✨", url=f"https://t.me/shreya_ghoshal_143"), 
+                text="🥀 ❰ 𝐎𝐰𝐧𝐞𝐫シ︎𝐱𝐃 ❱ ✨", url=f"https://t.me/ForeverMine102"), 
         ],
         [
             InlineKeyboardButton(
